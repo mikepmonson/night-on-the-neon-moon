db7a4fcd2b71ea801c8e7654e552dbe93c6cf95f
Kyle Diablo <kylediablo@comcast.net>
2018-10-08 13:01:20 -0700

    Added class Enemy

---
+++ Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable {
+    
+    public enum EnemyState
+    {
+        Entering,
+        Moving,
+        Attacking,
+        Dying
+    }
+
+    public int Health
+    {
+        get; set;    
+    }
+
+    public float MoveSpeed
+    {
+        get; set;
+    }
+
+    public EnemyState CurrentState
+    {
+        get; set;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        this.Health -= amount;
+    }
+}
