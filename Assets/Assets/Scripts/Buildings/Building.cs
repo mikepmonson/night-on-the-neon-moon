b081122c97e208037b3158e94f2a276e417e533e
Kyle Diablo <kylediablo@comcast.net>
2018-10-12 13:00:49 -0700

    Finished Braindead script

---
+++ Assets/Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour, IDamageable {
+
+    public int Health { get; set; }
+    [SerializeField]
+    private Sprite _destroyedSprite;
+
+    // Use this for initialization
+    void Start () {
+        Health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage(int amount)
+    {
+        this.Health -= amount;
+        Debug.Log(this.name + " took " + amount + " damage.");
+        Debug.Log(this.name + " Health: " + this.Health);
+        if (this.Health <= 0)
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = _destroyedSprite;
+    }
+}
--- Assets/Assets/Scripts/Enemy/Braindead.cs
+++ Assets/Assets/Scripts/Enemy/Braindead.cs
@@ -1,28 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
-     * Todo: 
-     * Add public properties: GameObject target, bool chasingPlayer, float buildingAttackCooldown
-     * Add public methods: void Aggro(), void TargetNearestBuilding()
-     * On update:
 
-        If state is entering:
-        Move towards target. If at target, acquire nearest building as target (TargetNearestBuilding) and enter moving state.
-
-        If state is dying:
-        Disable collider and rigidbody. Play death animation. Destroy object.
-
-        Check health. If 0, enter dying state.
-
-        If state is moving:
-            If not ChasingPlayer:
-                Check if building is in range. If so, enter attacking state.
-        If state is attacking:
-            If attack is not on cooldown, damage building. Otherwise, do nothing.
-        On collision:
-            If collision target is tagged as player, make them receive damage (IDamageable)   
-    */
 public class Braindead : Enemy, IDamageable{
 
     //Properties
@@ -33,18 +12,22 @@ public class Braindead : Enemy, IDamageable{
     public override int Health { get; set; }
     public override float MoveSpeed { get; set; }
     public override EnemyState CurrentState { get; set; }
+    public float LastAttacked { get; set; }
 
     //THIS FIELD IS ONLY FOR TESTING UNTIL GAMEMANAGER IS COMPLETE
-    public GameObject[] buildings;
+    [SerializeField]
+    public List<GameObject> buildings;
     //END TESTING FIELD
 
 
     // Use this for initialization
     void Start () {
+        LastAttacked = 0f;
         BuildingAttackCooldown = 2f;
         AggroRange = 1.5f;
         MoveSpeed = 2f;
-        buildings = GameObject.FindGameObjectsWithTag("Building");
+        GameObject[] buildingsArr = GameObject.FindGameObjectsWithTag("Building");
+        buildings = new List<GameObject>(buildingsArr);
         CurrentState = EnemyState.Moving;
         Target = GameObject.FindGameObjectWithTag("Player");
         Debug.Log("Started");
@@ -52,16 +35,14 @@ public class Braindead : Enemy, IDamageable{
 	
 	// Update is called once per frame
 	void Update () {
-        //check status
-
-        Debug.Log("Checking status...");
-        CheckStatus();
-        Debug.Log("Checked status...");
-        //check aggro
+        if (buildings.Count <= 0)
+            Destroy(this.gameObject);
         Aggro();
-        //move toward target
-        //if at target, attack target
-	}
+        //check status      
+        CheckStatus();
+        //check aggro
+        
+    }
 
     private void CheckStatus()
     {
@@ -71,15 +52,12 @@ public class Braindead : Enemy, IDamageable{
         }
         else if(CurrentState == EnemyState.Moving)
         {
-            Debug.Log("Status is moving");
             //move towards target
-            Debug.Log("moving...");
             this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, Target.transform.position, MoveSpeed * Time.deltaTime);
-            Debug.Log("Moved.");
         }
         else if (CurrentState == EnemyState.Attacking)
         {
-
+            Attack();
         }
         else if (CurrentState == EnemyState.Dying)
         {
@@ -95,50 +73,76 @@ public class Braindead : Enemy, IDamageable{
             Destroy(this.gameObject);
         }
     }
-    /// <summary>
-    /// This method is triggered if another object with a collider enters this collider. This will check if the player collided with either the player or a projectile.
-    /// </summary>
-    /// <param name="other object that triggered this"></param>
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            // Syntax:
-            // ScriptName variableName = other.GetComponent<ScriptName>();
-            // if(variableName != null)
-            //     variableName.TakeDamage(damageAmount);
-            //---------------------------------------------------------------
-            // Player player = other.GetComponent<Player>();
-            // if(player != null)
-            //     player.TakeDamage(1);
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+                player.TakeDamage(1);
+        }
+        else if (other.tag == "Building")
+        {
+            CurrentState = EnemyState.Attacking;
         }
     }
 
     public void Aggro()
     {
-        //if the difference between the player position and this gameObject position is <= .5f, target the player 
+        //if the difference between the player position and this gameObject position is in range, target the player 
         if (Vector3.Distance(this.gameObject.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= AggroRange)
         {
             Target = GameObject.FindGameObjectWithTag("Player");
             ChasingPlayer = true;
+            if (CurrentState != EnemyState.Moving)
+                CurrentState = EnemyState.Moving;
         }
         else
         {
-            TargetNearestBuilding();
+            Target = FindNearestBuilding();
+            ChasingPlayer = false;     
         }
     }
 
-    public void TargetNearestBuilding()
+    public GameObject FindNearestBuilding()
     {
-        GameObject closestBuilding = buildings[0];
-        foreach (GameObject bldg in buildings)
-        {
-            if (Vector3.Distance(this.transform.position, bldg.transform.position) < Vector3.Distance(this.transform.position, closestBuilding.transform.position))
+            GameObject closestBuilding = buildings[0];
+            foreach (GameObject bldg in buildings)
             {
-                closestBuilding = bldg;
+                if (Vector3.Distance(this.transform.position, bldg.transform.position) < Vector3.Distance(this.transform.position, closestBuilding.transform.position))
+                {
+                    closestBuilding = bldg;
+                }
+            }
+            return closestBuilding;      
+    }
+
+    private void Attack()
+    {
+        if (!ChasingPlayer && CanAttack() == true)
+        {
+            if (Target.tag == "Building")
+            {
+                Building bldg = Target.GetComponent<Building>();
+                bldg.TakeDamage(1);
+                Debug.Log(this.name + " is attacking " + bldg.name);
+                LastAttacked = Time.time;
+                if (bldg.Health <= 0)
+                {
+                    buildings.Remove(Target);
+                    CurrentState = EnemyState.Moving;
+                }
             }
         }
-        Target = closestBuilding;
+    }
+
+    private bool CanAttack()
+    {
+        if (LastAttacked + BuildingAttackCooldown <= Time.time)
+            return true;
+        else
+            return false;
     }
 
     public override void TakeDamage(int amount)
