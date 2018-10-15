f6eead8909a6fe94417ced038eef90574099cb88
Kyle Diablo <kylediablo@comcast.net>
2018-10-15 11:43:15 -0700

    Added documentation to EnemyProjectile and Shooter scripts

--- Assets/Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Assets/Scripts/Enemy/Shooter.cs
@@ -12,7 +12,9 @@ public class Shooter : Enemy, IDamageable {
     private float _lastFireTime;
     private float _fireRange;
 
-    // Use this for initialization
+    /// <summary>
+    /// Initializes cooldown, last fire time, fire range, enemy state, move speed, and health
+    /// </summary>
     void Start () {
         _fireCooldown = 2f;
         _lastFireTime = 0;
@@ -22,20 +24,18 @@ public class Shooter : Enemy, IDamageable {
         Health = 3;
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+    /// Every game tick, this will check the enemy state, and calls the Fire() method
+    /// </summary>
 	void Update () {
         if (CurrentState == EnemyState.Entering)
         {
-
+            //not being used at the moment
         }
         else if (CurrentState == EnemyState.Moving)
         {
             //move towards target
             this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, MoveSpeed * Time.deltaTime);
-        }
-        else if (CurrentState == EnemyState.Attacking)
-        {
-            
         }
         else if (CurrentState == EnemyState.Dying)
         {
@@ -46,7 +46,7 @@ public class Shooter : Enemy, IDamageable {
                 Destroy(this.GetComponent<BoxCollider2D>());
             }
             //play death anim
-            //can skip for now
+                //can skip for now
             //destroy gameobject
             Destroy(this.gameObject);
         }
@@ -54,6 +54,9 @@ public class Shooter : Enemy, IDamageable {
         Fire();
     }
 
+    /// <summary>
+    /// If the CanFire() method returns true, and this object is within range of the player, instantiate projectile and update last fired
+    /// </summary>
     private void Fire()
     {
         if (CanFire() && Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= _fireRange)
@@ -64,15 +67,23 @@ public class Shooter : Enemy, IDamageable {
         
     }
 
+    /// <summary>
+    /// reduce health by the amount taken, enter Dying state if health is les sthan or equal to zero
+    /// </summary>
+    /// <param name="amount">the amount of damage to be administered</param>
     public override void TakeDamage(int amount)
     {
-        this.Health -= amount;
+        this.Health -= amount;//reduce health by amount
         if (this.Health <= 0)
         {
-            CurrentState = EnemyState.Dying;
+            CurrentState = EnemyState.Dying;//if health is <= 0, enter Dying state
         }
     }
 
+    /// <summary>
+    /// if this object collides with player, make player take damage
+    /// </summary>
+    /// <param name="other">object this collided with</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
@@ -83,6 +94,10 @@ public class Shooter : Enemy, IDamageable {
         }
     }
 
+    /// <summary>
+    /// will return the result of the last time fired + the cooldown compared to the current game time
+    /// </summary>
+    /// <returns>true if the cooldown has been satisfied</returns>
     private bool CanFire()
     {
         if(_lastFireTime + _fireCooldown <= Time.time)
--- Assets/Assets/Scripts/Projectile/EnemyProjectile.cs
+++ Assets/Assets/Scripts/Projectile/EnemyProjectile.cs
@@ -8,9 +8,12 @@ public class EnemyProjectile : MonoBehaviour {
     private float _velocity; //speed of the projectile
     [SerializeField]
     private float _lifetime; //how long the projectile GameObject stays alive
-    public float birth;
-    private GameObject target;
+    public float birth; //the gametime that this object is instantiated
+    private GameObject target; //the target of this projectile (player)
 
+    /// <summary>
+    /// Sets the rotation of this projectile to face the player
+    /// </summary>
     private void Awake()
     {
         target = GameObject.FindGameObjectWithTag("Player");
@@ -21,14 +24,18 @@ public class EnemyProjectile : MonoBehaviour {
                                offset           // Point y+ toward the target.
                              );
     }
-    // Use this for initialization
+    /// <summary>
+    /// Sets the velocity, projectile lifetime, and birth time
+    /// </summary>
     void Start () {
         _velocity = 5f;
         _lifetime = 5.0f;
         birth = Time.time;
     }
 	
-	// Update is called once per frame
+	/// <summary>
+    /// Every tick, calls Movement() method and destroys the gameobject if it has existed over _lifetime
+    /// </summary>
 	void Update () {
         Movement();
         if (Time.time - birth >= _lifetime)
@@ -36,13 +43,18 @@ public class EnemyProjectile : MonoBehaviour {
             Destroy(this.gameObject);
         }
     }
-
+    /// <summary>
+    /// moves towards the initial direction set upon instantiation
+    /// </summary>
     private void Movement()
     {
         this.transform.Translate(new Vector3(0, 1, 0) * _velocity * Time.deltaTime);
 
     }
-
+    /// <summary>
+    /// if this collides with the player, damage the player
+    /// </summary>
+    /// <param name="other">the object this collided with</param>
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
