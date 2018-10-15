fa4168824165e96042ff406507ab5a06a95e02ff
Kyle Diablo <kylediablo@comcast.net>
2018-10-15 11:29:12 -0700

    Added Shooter and Enemy Projectile classes. Shooter is fully functional as well as EnemyProjectile

--- Assets/Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Assets/Scripts/Enemy/Shooter.cs
@@ -2,15 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour {
+public class Shooter : Enemy, IDamageable {
+    public override int Health { get; set; }
+    public override float MoveSpeed { get; set; }
+    public override EnemyState CurrentState { get; set; }
+    [SerializeField]
+    private GameObject _projectile;
+    private float _fireCooldown;
+    private float _lastFireTime;
+    private float _fireRange;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        _fireCooldown = 2f;
+        _lastFireTime = 0;
+        _fireRange = 4f;
+        CurrentState = EnemyState.Moving;
+        MoveSpeed = 2.1f;
+        Health = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (CurrentState == EnemyState.Entering)
+        {
+
+        }
+        else if (CurrentState == EnemyState.Moving)
+        {
+            //move towards target
+            this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, MoveSpeed * Time.deltaTime);
+        }
+        else if (CurrentState == EnemyState.Attacking)
+        {
+            
+        }
+        else if (CurrentState == EnemyState.Dying)
+        {
+            //disable rigidbody and collider
+            if (this.GetComponent<Rigidbody2D>() != null && this.GetComponent<BoxCollider2D>() != null)
+            {
+                Destroy(this.GetComponent<Rigidbody2D>());
+                Destroy(this.GetComponent<BoxCollider2D>());
+            }
+            //play death anim
+            //can skip for now
+            //destroy gameobject
+            Destroy(this.gameObject);
+        }
+
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (CanFire() && Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= _fireRange)
+        {
+            Instantiate(_projectile, this.transform.position, Quaternion.identity);
+            _lastFireTime = Time.time;
+        }
+        
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        this.Health -= amount;
+        if (this.Health <= 0)
+        {
+            CurrentState = EnemyState.Dying;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+                player.TakeDamage(1);
+        }
+    }
+
+    private bool CanFire()
+    {
+        if(_lastFireTime + _fireCooldown <= Time.time)
+            return true;
+        
+        else
+            return false;
+    }
 }
---
+++ Assets/Assets/Scripts/Projectile/EnemyProjectile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour {
+
+    [SerializeField]
+    private float _velocity; //speed of the projectile
+    [SerializeField]
+    private float _lifetime; //how long the projectile GameObject stays alive
+    public float birth;
+    private GameObject target;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        Vector3 offset = target.transform.position - this.transform.position;
+
+        transform.rotation = Quaternion.LookRotation(
+                               Vector3.forward, // Keep z+ pointing straight into the screen.
+                               offset           // Point y+ toward the target.
+                             );
+    }
+    // Use this for initialization
+    void Start () {
+        _velocity = 5f;
+        _lifetime = 5.0f;
+        birth = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        if (Time.time - birth >= _lifetime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Movement()
+    {
+        this.transform.Translate(new Vector3(0, 1, 0) * _velocity * Time.deltaTime);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player p = other.GetComponent<Player>();
+            p.TakeDamage(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
